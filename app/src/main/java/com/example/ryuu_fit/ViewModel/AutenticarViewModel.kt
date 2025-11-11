52041552e0a6eb29b2cfbb01b9c8ecedcbb1c8d8
OPG23 <opajaro01@uan.edu.co>
2025-11-10 23:23:01 -0500

    Probando Conexion con DB Probando Again

---
+++ app/src/main/java/com/example/ryuu_fit/ViewModel/AutenticarViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.ryuu_fit.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ryuu_fit.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+
+class AutenticarViewModel {
+
+    /*
+    val googleIdOption = GetGoogleIdOption.Builder()
+        .setFilterByAuthorizedAccounts(true)
+        .setServerClientId(baseContext.getString(R.string.default_web_client_id))
+        .build()
+
+    val request = GetCredentialRequest.Builder()
+        .addCredentialOption(googleIdOption)
+        .build()
+
+     */
+}
